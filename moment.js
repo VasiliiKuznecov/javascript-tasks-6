bd9c7726a743ad4f6b187863b37bbfa32c4d2a44
Vasilii <kuznecov_vasilii@.mail.ru>
2015-11-09 00:35:45 +0500

    сделал задание

--- moment.js
+++ moment.js
@@ -1,20 +1,95 @@
 'use strict';
 
-module.exports = function () {
+var initDate = new Date();
+
+var MILISECONDS_PER_DAY = 1000 * 60 * 60 * 24;
+var MILISECONDS_PER_HOUR = 1000 * 60 * 60;
+var MILISECONDS_PER_MINUTE = 1000 * 60;
+
+module.exports = function (_date) {
     return {
         // Здесь как-то хранится дата ;)
-        date: null,
+        dateObject: makeDateObject(_date),
 
         // А здесь часовой пояс
-        timezone: null,
+        timezone: 5,
 
-        // Выводит дату в переданном формате
+        set date(_date) {
+            this.dateObject = makeDateObject(_date);
+        },
+
+        get date() {
+            return this.dateObject;
+        },
+
         format: function (pattern) {
+            var formatedDate = pattern;
+            var days = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
+            formatedDate = formatedDate.replace(/%DD/, days[this.dateObject.getDay()]);
+            formatedDate = formatedDate.replace(
+                /%HH/, addLeadZero(this.dateObject.getUTCHours() + this.timezone)
+            );
+            formatedDate = formatedDate.replace(
+                /%MM/, addLeadZero(this.dateObject.getUTCMinutes())
+            );
+            return formatedDate;
         },
 
         // Возвращает кол-во времени между текущей датой и переданной `moment`
         // в человекопонятном виде
         fromMoment: function (moment) {
+            var milisecondsLeft = this.date.getTime() - moment.date.getTime();
+            var timeLeft = {};
+            timeLeft['days'] = Number.parseInt(milisecondsLeft / MILISECONDS_PER_DAY);
+            milisecondsLeft = milisecondsLeft % MILISECONDS_PER_DAY;
+            timeLeft['hours'] = Number.parseInt(milisecondsLeft / MILISECONDS_PER_HOUR);
+            milisecondsLeft = milisecondsLeft % MILISECONDS_PER_HOUR;
+            timeLeft['minutes'] = Number.parseInt(milisecondsLeft / MILISECONDS_PER_MINUTE);
+            return 'До ограбления: Дней: ' + timeLeft['days'] + ' Часов: ' + timeLeft['hours'] +
+                ' Минут: ' + timeLeft['minutes'];
         }
     };
+};
+
+module.exports.makeDate = function (time) {
+    var parsedTime = {
+        day: time.substr(0, 2),
+        hours: Number.parseInt(time.substr(3, 2)),
+        minutes: Number.parseInt(time.substr(6, 2)),
+        timezone: Number.parseInt(time.substr(8))
+    };
+    var date = new Date(initDate.getTime());
+    var days = {
+        'ВС': 0,
+        'ПН': 1,
+        'ВТ': 2,
+        'СР': 3,
+        'ЧТ': 4,
+        'ПТ': 5,
+        'СБ': 6
+    };
+    var untilTimestampDay = (7 - initDate.getUTCDay() + days[parsedTime.day]) % 7;
+    date.setUTCDate(initDate.getUTCDate() + untilTimestampDay);
+    date.setUTCHours(parsedTime.hours - parsedTime.timezone, parsedTime.minutes, 0, 0);
+
+    return date;
+};
+
+function addLeadZero(number) {
+    if (number < 10) {
+        return '0' + number;
+    } else {
+        return number.toString();
+    }
+}
+
+function makeDateObject(_date) {
+    if (!_date) {
+        return;
+    }
+    if (_date instanceof Date) {
+        return _date;
+    } else {
+        return module.exports.makeDate(_date);
+    }
 };
--- robbery.js
+++ robbery.js
@@ -2,18 +2,67 @@
 
 var moment = require('./moment');
 
-// Выбирает подходящий ближайший момент начала ограбления
 module.exports.getAppropriateMoment = function (json, minDuration, workingHours) {
     var appropriateMoment = moment();
 
-    // 1. Читаем json
-    // 2. Находим подходящий ближайший момент начала ограбления
-    // 3. И записываем в appropriateMoment
+    var gangShedule = JSON.parse(json);
+    var timestamps = [];
+    Object.keys(gangShedule).forEach(function (gangMember) {
+        gangShedule[gangMember].forEach(function (timeInterval) {
+            timestamps.push(
+                {
+                    participant: gangMember,
+                    moment: moment(timeInterval.from),
+                    state: 'busy'
+                },
+                {
+                    participant: gangMember,
+                    moment: moment(timeInterval.to),
+                    state: 'free'
+                }
+            );
+        });
+    });
 
-    return appropriateMoment;
+    ['ПН','ВТ','СР'].forEach(function (day) {
+        timestamps.push(
+            {
+                participant: 'bank',
+                moment: moment(day + ' ' + workingHours.from + '+5'),
+                state: 'free'
+            },
+            {
+                participant: 'bank',
+                moment: moment(day + ' ' + workingHours.to + '+5'),
+                state: 'busy'
+            }
+        );
+    });
+
+    timestamps.sort(function (timestamp1, timestamp2) {
+        return timestamp1.moment.date > timestamp2.moment.date ? 1 : -1;
+    });
+
+    var participantsNumber = Object.keys(gangShedule).length + 1;
+    var participantsFree = Object.keys(gangShedule).length;
+    for (var i = 0; i < timestamps.length; i++) {
+        if (timestamps[i].state === 'busy') {
+            participantsFree--;
+        } else {
+            participantsFree++;
+        }
+        if (participantsFree === participantsNumber) {
+            var timeForRobbery =
+                timestamps[i + 1].moment.date.getTime() - timestamps[i].moment.date.getTime();
+            if (timeForRobbery >= minDuration * 60000) {
+                appropriateMoment = timestamps[i].moment;
+                return appropriateMoment;
+            }
+        }
+    }
+    return;
 };
 
-// Возвращает статус ограбления (этот метод уже готов!)
 module.exports.getStatus = function (moment, robberyMoment) {
     if (moment.date < robberyMoment.date) {
         // «До ограбления остался 1 день 6 часов 59 минут»
